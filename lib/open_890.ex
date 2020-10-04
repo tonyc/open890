9761640c57800823fb590db4d523d96083fd73e3
Tony Collen <tony@halogenlabs.com>
2020-10-04 10:09:20 -0500

    Make multi-ch button work with hooks, wire up some other basic stuff

--- lib/open_890.ex
+++ lib/open_890.ex
@@ -7,13 +7,36 @@ defmodule Open890 do
   if it comes from the database, an external API or others.
   """
 
-  def ch_up, do: "CH0" |> send_command()
-  def ch_down, do: "CH1" |> send_command()
+  def ch_up, do: "CH0" |> cmd()
+  def ch_down, do: "CH1" |> cmd()
 
-  def radio_up(args \\ "04") when is_binary(args), do: "UP#{args}" |> send_command()
-  def radio_down(args \\ "04") when is_binary(args), do: "DN#{args}" |> send_command()
+  def radio_up(args \\ "04") when is_binary(args), do: "UP#{args}" |> cmd()
+  def radio_down(args \\ "04") when is_binary(args), do: "DN#{args}" |> cmd()
 
-  def send_command(cmd) when is_binary(cmd) do
+  def freq_change(:up) do
+    ("FC0" <> freq_change_step())
+    |> cmd()
+  end
+
+  def freq_change(:down) do
+    ("FC1" <> freq_change_step())
+    |> cmd()
+  end
+
+  def vfo_a_b_swap, do: "EC" |> cmd()
+
+  def get_vfo_a_freq do
+    "FA" |> cmd()
+  end
+
+  def get_vfo_b_freq do
+    "FB" |> cmd()
+  end
+
+  # TODO: Make this configurable
+  defp freq_change_step, do: "4"
+
+  def cmd(cmd) when is_binary(cmd) do
     Open890.TCPClient.cmd(cmd)
   end
 
--- lib/open_890_web/live/radio_live.ex
+++ lib/open_890_web/live/radio_live.ex
@@ -13,28 +13,45 @@ defmodule Open890Web.RadioLive do
       Phoenix.PubSub.subscribe(Open890.PubSub, "radio:state")
     end
 
+    Open890.get_vfo_a_freq()
+    Open890.get_vfo_b_freq()
+
+
     {:ok,
       socket
         |> assign(:s_meter, "0")
         |> assign(:vfo_a_frequency, "FA00000000000")
+        |> assign(:vfo_b_frequency, "FB00000000000")
     }
   end
 
   @impl true
-  def handle_event("mic_up", _meta, socket) do
+  def handle_event("mic_up", _params, socket) do
     Logger.info("Event: mic_up")
     Open890.ch_up()
-
-    # This should look something like:
-    # Radio.mic_up()
 
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("mic_dn", _meta, socket) do
+  def handle_event("mic_dn", _params, socket) do
     Logger.info("Event: mic_dn")
     Open890.ch_down()
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("multi_ch", %{"is_up" => is_up} = params, socket) do
+    Logger.info("Event: multi/ch, meta: #{inspect(params)}")
+
+    direction = if is_up do
+      :up
+    else
+      :down
+    end
+
+    Open890.freq_change(direction)
 
     {:noreply, socket}
   end
@@ -49,6 +66,12 @@ defmodule Open890Web.RadioLive do
 
       msg |> String.starts_with?("FA") ->
         {:noreply, socket |> assign(:vfo_a_frequency, msg)}
+
+      msg |> String.starts_with?("FB") ->
+        {:noreply, socket |> assign(:vfo_b_frequency, msg)}
+
+
+
       true ->
         {:noreply, socket}
     end
