650990bfc9eadf4dcd0c8c1e12535b94bd5f1930
Tony Collen <tony@halogenlabs.com>
2020-10-22 21:46:53 -0500

    Allow options to be passed in cmd_button

--- lib/open_890_web/radio_view_helpers.ex
+++ lib/open_890_web/radio_view_helpers.ex
@@ -5,14 +5,14 @@ defmodule Open890Web.RadioViewHelpers do
     if theme == name, do: "selected"
   end
 
-  def cmd_button(name, cmd) when is_binary(name) and is_binary(cmd) do
-    content_tag(:button, name, phx_click: "cmd", phx_value_cmd: cmd)
+  def cmd_button(name, cmd, opts \\ []) when is_binary(name) and is_binary(cmd) do
+    content_tag(:button, name, opts |> Keyword.merge(phx_click: "cmd", phx_value_cmd: cmd))
   end
 
   def format_band_scope_mode(mode) do
     mode
     |> case do
-      :auto_scroll -> "Auto-Scroll"
+      :auto_scroll -> "Auto Scroll"
       :fixed -> "Fixed"
       :center -> "Center"
       _ -> ""
