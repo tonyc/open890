9e65ee6337db3daface336c6f34d52476e26ac73
Tony Collen <tony@halogenlabs.com>
2020-11-12 21:53:00 -0600

    Disable ecto repo in test

--- config/test.exs
+++ config/test.exs
@@ -5,12 +5,6 @@ use Mix.Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :open_890, Open890.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "open_890_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
