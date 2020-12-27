a0a416ec22f9c676d8dd85c289fed6c0c60e6d6d
Tony Collen <tony@halogenlabs.com>
2020-12-27 13:07:30 -0600

    Make releases work, add bakeware

--- mix.exs
+++ mix.exs
@@ -1,9 +1,10 @@
 defmodule Open890.MixProject do
   use Mix.Project
+  @app :open_890
 
   def project do
     [
-      app: :open_890,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -18,7 +19,15 @@ defmodule Open890.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.json": :test
-      ]
+      ],
+      releases: [{@app, release()}]
+    ]
+  end
+
+  defp release do
+    [
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod,
     ]
   end
 
@@ -41,24 +50,25 @@ defmodule Open890.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bakeware, "~> 0.1.4"},
       {:credo, "~> 1.5", only: [:dev, :test]},
-      {:excoveralls, "~> 0.13.3", only: [:dev, :test]},
       {:ecto_sql, "~> 3.4"},
       {:elixir_math, "~> 0.1.2"},
+      {:excoveralls, "~> 0.13.3", only: [:dev, :test]},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:postgrex, ">= 0.0.0"},
+      {:observer_cli, "~> 1.5"},
       {:phoenix, "~> 1.5.4"},
       {:phoenix_ecto, "~> 4.1"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_view, "0.15.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4.0"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "0.15.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:sobelow, "~> 0.10.5", only: [:dev, :test]},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:observer_cli, "~> 1.5"},
-      {:plug_cowboy, "~> 2.0"},
-      {:sobelow, "~> 0.10.5", only: [:dev, :test]}
     ]
   end
 
