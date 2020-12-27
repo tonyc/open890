500a1a5161eef786172a60ac82eeeb85c34dc5d4
Tony Collen <tony@halogenlabs.com>
2020-12-27 12:10:14 -0600

    Release sorta works?

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :open_890, Open890Web.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 4000],
+  http: [port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+#database_url =
+#  System.get_env("DATABASE_URL") ||
+#    raise """
+#    environment variable DATABASE_URL is missing.
+#    For example: ecto://USER:PASS@HOST/DATABASE
+#    """
 
-config :open_890, Open890.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+#config :open_890, Open890.Repo,
+#  # ssl: true,
+#  url: database_url,
+#  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -26,8 +26,10 @@ secret_key_base =
 config :open_890, Open890Web.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
+    transport_options: [socket_opts: [:inet6]],
   ],
+  url: [host: "localhost", port: 4000],
+  server: true,
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
