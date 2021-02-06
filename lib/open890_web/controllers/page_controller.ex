3ffe9b239e92540032339b1e06b5382d8ef314f5
Tony Collen <tony@halogenlabs.com>
2021-02-05 19:38:21 -0600

    Just redirect to the connections list for now

--- lib/open890_web/controllers/page_controller.ex
+++ lib/open890_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule Open890Web.PageController do
   use Open890Web, :controller
 
   def index(conn, _params) do
-    conn |> render("index.html")
+    conn
+    |> redirect(to: "/connections")
+    |> halt()
+
+    # conn |> render("index.html")
   end
 end
