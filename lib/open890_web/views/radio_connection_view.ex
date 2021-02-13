981d78334ef2364cb14e43a19029dd37f7e519e1
Tony Collen <tony@halogenlabs.com>
2021-02-13 12:22:49 -0600

    More manage connection index updates

--- lib/open890_web/views/radio_connection_view.ex
+++ lib/open890_web/views/radio_connection_view.ex
@@ -1,4 +1,8 @@
 defmodule Open890Web.RadioConnectionView do
   use Open890Web, :view
   alias Open890.RadioConnection
+
+  def connection_to_uri(%RadioConnection{} = c) do
+    "#{c.type}://#{c.user_name}@#{c.ip_address}"
+  end
 end
