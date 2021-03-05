a70d72df1ff26947746c78a8be8b7ce164bdc6dc
Tony Collen <tony@halogenlabs.com>
2021-03-05 16:43:18 -0600

    Add the mode switcher to the band change dialog

--- lib/open890_web/live/band_buttons_component.ex
+++ lib/open890_web/live/band_buttons_component.ex
@@ -3,26 +3,46 @@ defmodule Open890Web.Live.BandButtonsComponent do
 
   def render(assigns) do
     ~L"""
-      <div class="ui equal width grid">
+      <div class="ui grid">
         <div class="row">
-          <div class="column"><%= cmd_button "1.8", "BU000", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "3.5", "BU001", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "7", "BU002", class: "large fluid" %></div>
-        </div>
-        <div class="row">
-          <div class="column"><%= cmd_button "10", "BU003", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "14", "BU004", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "18", "BU005", class: "large fluid" %></div>
-        </div>
-        <div class="row">
-          <div class="column"><%= cmd_button "21", "BU006", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "24", "BU007", class: "large fluid" %></div>
-          <div class="column"><%= cmd_button "28", "BU008", class: "large fluid" %></div>
-        </div>
-        <div class="row">
-          <div class="column"><%= cmd_button "GEN", "BU010", class: "large fluid" %></div>
-          <div class="column"></div>
-          <div class="column"><%= cmd_button "50", "BU009", class: "large fluid" %></div>
+          <div class="four wide column center aligned aligned">
+
+            <div id="modeKeys" class="ui vertical buttons">
+              <%= cmd_button "LSB/USB",   "MK0", class: "large fluid" %>
+              <%= cmd_button "CW/CW-R",   "MK1", class: "large fluid" %>
+              <%= cmd_button "FSK/PSK",   "MK2", class: "large fluid" %>
+              <%= cmd_button "FM/AM",     "MK3", class: "large fluid" %>
+              <%= cmd_button "FSK/FSK-R", "MK4", class: "large fluid" %>
+              <%= cmd_button "PSK/PSK-R", "MK5", class: "large fluid" %>
+            </div>
+
+          </div>
+          <div class="twelve wide column">
+
+            <div class="ui equal width grid">
+              <div class="row">
+                <div class="column"><%= cmd_button "1.8", "BU000", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "3.5", "BU001", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "7", "BU002", class: "large fluid" %></div>
+              </div>
+              <div class="row">
+                <div class="column"><%= cmd_button "10", "BU003", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "14", "BU004", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "18", "BU005", class: "large fluid" %></div>
+              </div>
+              <div class="row">
+                <div class="column"><%= cmd_button "21", "BU006", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "24", "BU007", class: "large fluid" %></div>
+                <div class="column"><%= cmd_button "28", "BU008", class: "large fluid" %></div>
+              </div>
+              <div class="row">
+                <div class="column"><%= cmd_button "GEN", "BU010", class: "large fluid" %></div>
+                <div class="column"></div>
+                <div class="column"><%= cmd_button "50", "BU009", class: "large fluid" %></div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     """
