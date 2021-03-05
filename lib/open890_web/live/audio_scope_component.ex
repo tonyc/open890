b6a019a7ed41276ff9d9cdcf3c8493a066587a94
Tony Collen <tony@halogenlabs.com>
2021-03-04 21:19:52 -0600

    Display basic filter info: active IF filter, lo/width, hi_shift

--- lib/open890_web/live/audio_scope_component.ex
+++ lib/open890_web/live/audio_scope_component.ex
@@ -3,8 +3,8 @@ defmodule Open890Web.Live.AudioScopeComponent do
 
   def render(assigns) do
     ~L"""
-      <div id="audioScopeWrapper" class="hover-pointer">
-        <svg id="audioScope" class="scope themed <%= @theme %>" viewbox="0 0 212 50" phx-hook="AudioScope">
+      <div id="audioScopeWrapper" class="hover-pointer _debug">
+        <svg id="audioScope" class="scope themed <%= @theme %>" viewbox="0 0 212 60" phx-hook="AudioScope">
           <defs>
             <linearGradient id="kenwood" gradientTransform="rotate(90)">
               <stop offset="0" stop-color="white" />
@@ -12,19 +12,57 @@ defmodule Open890Web.Live.AudioScopeComponent do
             </linearGradient>
           </defs>
 
-          <text><%= @active_if_filter %></text>
+          <g transform="translate(0 10)">
+            <polygon id="audioSpectrum" class="spectrum" points="<%= scope_data_to_svg(@audio_scope_data, 60) %>" vector-effect="non-scaling-stroke" />
 
-          <polygon id="audioSpectrum" class="spectrum" points="<%= scope_data_to_svg(@audio_scope_data, 50) %>" vector-effect="non-scaling-stroke" />
+            <%= if @active_if_filter && @roofing_filter_data[@active_if_filter] do %>
+              <%= audio_scope_filter_edges(@active_mode, {@filter_lo_width, @filter_hi_shift}, @active_if_filter, @roofing_filter_data) %>
+            <% end %>
 
-          <%= if @active_if_filter && @roofing_filter_data[@active_if_filter] do %>
-            <%= audio_scope_filter_edges(@active_mode, {@filter_lo_width, @filter_hi_shift}, @active_if_filter, @roofing_filter_data) %>
-          <% end %>
+            <line id="audioScopeTuneIndicator" class="primaryCarrier" x1="106" y1="5" x2="106" y2="60" />
+          </g>
 
-          <line id="audioScopeTuneIndicator" class="primaryCarrier" x1="106" y1="5" x2="106" y2="50" />
+          <g transform="translate(3 10)">
+            <text class="audioScopeLabel">
+              <%= @active_if_filter |> to_string |> String.upcase() %>
+            </text>
+
+            <g transform="translate(20 0)">
+              <text class="audioScopeLabel">
+                <%= filter_lo_width_label(@active_mode) %>: <%= @filter_lo_width %>
+              </text>
+            </g>
+
+            <g transform="translate(170 0)">
+              <text class="audioScopeLabel">
+                <%= if @active_mode in [:cw, :cw_r, :lsb, :lsb_d, :usb, :usb_d, :am, :fm] do %>
+                  <%= filter_hi_shift_label(@active_mode) %>: <%= @filter_hi_shift %>
+                <% end %>
+              </text>
+            </g>
+          </g>
+
         </svg>
 
-        <canvas phx-hook="AudioScopeCanvas" id="AudioScopeCanvas" data-theme="<%= @theme %>" class="waterfall" width="213" height="50"></canvas>
+        <canvas phx-hook="AudioScopeCanvas" id="AudioScopeCanvas" data-theme="<%= @theme %>" class="waterfall" width="213" height="60"></canvas>
       </div>
     """
+  end
+
+
+  def filter_lo_width_label(mode) when mode in [:cw, :cw_r, :fsk, :fsk_r, :psk, :psk_r] do
+    "WIDTH"
+  end
+
+  def filter_lo_width_label(_) do
+    "LC"
+  end
+
+  def filter_hi_shift_label(mode) when mode in [:cw, :cw_r] do
+    "SHIFT"
+  end
+
+  def filter_hi_shift_label(_) do
+    "HC"
   end
 end
