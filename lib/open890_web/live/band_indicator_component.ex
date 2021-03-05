a13cc7ead84af305d487fb8afb136ae1c245d785
Tony Collen <tony@halogenlabs.com>
2021-03-05 09:08:31 -0600

    Extract the band indicator to a component

---
+++ lib/open890_web/live/band_indicator_component.ex
@@ -0,0 +1,34 @@
+defmodule Open890Web.Live.BandIndicatorComponent do
+
+  use Open890Web, :live_component
+
+  def render(assigns) do
+    ~L"""
+      <div class="bandIndicator">
+        <%= if @active_receiver == :a do %>
+          <div>
+            <span class="bandPointer">◀</span>
+            <span class="receiverIndicator active">A</span>
+            <span class="bandPointer"> </span>
+          </div>
+          <div>
+            <span class="bandPointer"></span>
+            <span class="receiverIndicator">B</span>
+            <span class="bandPointer">▶</span>
+          </div>
+        <% else %>
+          <div>
+            <span class="bandPointer"></span>
+            <span class="receiverIndicator">A</span>
+            <span class="bandPointer">▶</span>
+          </div>
+          <div>
+            <span class="bandPointer">◀</span>
+            <span class="receiverIndicator active">B</span>
+            <span class="bandPointer"> </span>
+          </div>
+        <% end %>
+      </div>
+    """
+  end
+end
--- lib/open890_web/live/vfo_display_component.ex
+++ lib/open890_web/live/vfo_display_component.ex
@@ -1,46 +1,18 @@
 defmodule Open890Web.Live.VFODisplayComponent do
   use Open890Web, :live_component
 
+  alias Open890Web.Live.BandIndicatorComponent
+
   def render(assigns) do
     ~L"""
-      <%= if @active_receiver == :a do %>
+      <div class="vfos">
         <%= vfo_mem_indicator(@vfo_memory_state) %>
         <%= mode_indicator(@active_mode) %>
         <div id="ActiveVFO" class="freq active"><%= @active_frequency |> format_raw_frequency() %></div>
-        <div class="bandIndicator">
-          <div class="receiver">
-            <span class="bandPointer">◀</span>
-            <span class="receiverIndicator active">A</span>
-            <span class="bandPointer"> </span>
-          </div>
-          <div>
-            <span class="bandPointer"></span>
-            <span class="receiverIndicator">B</span>
-            <span class="bandPointer">▶</span>
-          </div>
-        </div>
+        <%= live_component @socket, BandIndicatorComponent, active_receiver: @active_receiver %>
         <%= mode_indicator(@inactive_mode) %>
         <div class="freq inactive"><%= @inactive_frequency |> format_raw_frequency() %></div>
-      <% else %>
-        <%= vfo_mem_indicator(@vfo_memory_state) %>
-        <%= mode_indicator(@active_mode) %>
-        <div id="ActiveVFO" class="freq active"><%= @active_frequency |> format_raw_frequency() %></div>
-        <div class="bandIndicator">
-          <div class="receiver">
-            <span class="bandPointer"></span>
-            <span class="receiverIndicator">A</span>
-            <span class="bandPointer">▶</span>
-          </div>
-          <div>
-            <span class="bandPointer">◀</span>
-            <span class="receiverIndicator active">B</span>
-            <span class="bandPointer"> </span>
-          </div>
-        </div>
-        <%= mode_indicator(@inactive_mode) %>
-        <div class="freq inactive"><%= @inactive_frequency |> format_raw_frequency() %></div>
-      <% end %>
+      </div>
     """
   end
-
 end
