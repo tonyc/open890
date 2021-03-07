c114acc91ae80143a32ec9233c0a556e34eb1229
Tony Collen <tony@halogenlabs.com>
2021-03-07 15:24:37 -0600

    Attempt to pass ERLANG_VERSION and ELIXIR_VERSION through for windows builds

--- build_scripts/install_elixir.ps1
+++ build_scripts/install_elixir.ps1
@@ -1,7 +1,7 @@
 $WebClient = New-Object System.Net.WebClient
 
 Write-Output "Downloading Erlang"
-$erlang_url = "https://github.com/erlang/otp/releases/download/OTP-23.2.7/otp_win64_23.2.7.exe"
+$erlang_url = "https://github.com/erlang/otp/releases/download/OTP-23.2.7/otp_win64_$env:ERLANG_VERSION.exe"
 $WebClient.DownloadFile($erlang_url, "$env:TEMP\erlang.exe")
 
 Write-Output "Installing Erlang"
@@ -11,7 +11,7 @@ Write-Output "Waiting for c:\erlang\bin\erl.exe..."
 while (!(Test-Path "c:\erlang\bin\erl.exe")) { Start-Sleep 5 }
 
 Write-Output "Downloading Elixir"
-$WebClient.DownloadFile("https://github.com/elixir-lang/elixir/releases/download/v1.11.3/Precompiled.zip", "$env:TEMP\elixir.zip")
+$WebClient.DownloadFile("https://github.com/elixir-lang/elixir/releases/download/v$env:ELIXIR_VERSION/Precompiled.zip", "$env:TEMP\elixir.zip")
 
 Write-Output "Extracting Elixir"
 Expand-Archive -DestinationPath c:\elixir $env:TEMP\elixir.zip -Force
