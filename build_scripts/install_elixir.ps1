d1b81d79c1e50684954c8bb338796d303d9d55be
Tony Collen <tony@halogenlabs.com>
2021-03-07 12:44:01 -0600

    Let's just try a build script that I know works

---
+++ build_scripts/install_elixir.ps1
@@ -0,0 +1,18 @@
+$WebClient = New-Object System.Net.WebClient
+
+Write-Output "Downloading Erlang"
+$erlang_url = "https://github.com/erlang/otp/releases/download/OTP-23.2.7/otp_win64_23.2.7.exe"
+$WebClient.DownloadFile($erlang_url, "$env:TEMP\erlang.exe")
+
+Write-Output "Installing Erlang"
+$env:TEMP\erlang.exe /S /D=c:\erlang
+
+while (!(Test-Path "c:\erlang\bin\erl.exe")) { Start-Sleep 5 }
+
+Write-Output "Downloading Elixir"
+$WebClient.DownloadFile("https://github.com/elixir-lang/elixir/releases/download/v1.11.3/Precompiled.zip", "$env:TEMP\elixir.zip")
+
+Write-Output "Extracting Elixir"
+Expand-Archive -DestinationPath c:\elixir $env:TEMP\elixir.zip -Force
+
+Write-Output "Finished"
