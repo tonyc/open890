eb63506b59dfd3deea3085b27575433c1b44fee4
Tony Collen <tony@halogenlabs.com>
2021-09-05 10:50:27 -0500

    Removed separate buttons view

--- lib/open890_web/router.ex
+++ lib/open890_web/router.ex
@@ -25,7 +25,6 @@ defmodule Open890Web.Router do
     post "/connections/:id/stop", RadioConnectionController, :stop
 
     live "/connections/:id/bandscope", Live.Radio.Bandscope, :bandscope
-    live "/connections/:id/buttons", Live.RadioLive.Buttons, :buttons
     # live "/connections/:id/bandscope", Live.RadioLive.Bandscope
     # live "/connections/:id/buttons", Live.RadioButtonsLive, :index
   end
