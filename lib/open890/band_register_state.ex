1ec4ea3c6940720d59283a683fd3f22f80f43c4a
Tony Collen <tony@halogenlabs.com>
2021-09-06 14:10:05 -0500

    Display band register

--- lib/open890/band_register_state.ex
+++ lib/open890/band_register_state.ex
@@ -1,3 +1,13 @@
 defmodule Open890.BandRegisterState do
   defstruct vfo_a_band: nil, vfo_b_band: nil
+
+  def band_for(%__MODULE__{} = m, :a) do
+    m.vfo_a_band
+  end
+
+  def band_for(%__MODULE__{} = m, :b) do
+    m.vfo_b_band
+  end
+
+  def band_for(_, _), do: ""
 end
--- lib/open890_web/live/vfo_display_component.ex
+++ lib/open890_web/live/vfo_display_component.ex
@@ -2,28 +2,35 @@ defmodule Open890Web.Live.VFODisplayComponent do
   use Open890Web, :live_component
 
   alias Open890Web.Live.BandIndicatorComponent
+  alias Open890.BandRegisterState
 
   def render(assigns) do
     ~L"""
-      <div class="vfos ui stackable grid">
-        <div class="row">
-          <div class="eight wide column _debug">
+      <div class="vfos ui stackable compact grid">
+
+        <div class="row compact ">
+          <div class="seven wide right aligned column">
             <span class="vfoMemIndicator"><%= format_vfo_memory_state(@vfo_memory_state) %></span>
             <span class="modeIndicator active"><%= format_mode(@active_mode) %></span>
             <span class="freq active" phx-hook="ActiveVFO" id="ActiveVFO">
               <%= vfo_display_frequency(@active_frequency, @transverter_state) %>
             </span>
           </div>
-          <div class="left aligned eight wide column computer only tablet only _debug">
+          <div class="two wide center aligned column">
             <%= live_component @socket, BandIndicatorComponent, active_receiver: @active_receiver %>
+          </div>
+          <div class="left aligned seven wide column computer only tablet only">
             <span><%= format_mode(@inactive_mode) %></span>
             <div class="freq inactive"><%= vfo_display_frequency(@inactive_frequency, @transverter_state) %></div>
           </div>
         </div>
-        <div class="row">
-          <div class="column debug">
-            <p>VFO A Band: <%= @band_register_state.vfo_a_band %></p>
-            <p>VFO B Band: <%= @band_register_state.vfo_b_band %></p>
+        <div class="row ">
+          <div class="seven wide right aligned column">
+            <div class="bandRegister">BAND <span class="register"><%= band_for(@band_register_state, @active_receiver) %></span></div>
+          </div>
+          <div class="two wide column"> </div>
+          <div class="seven wide column">
+            <div class="bandRegister inactive">BAND <span class="register"><%= band_for(@band_register_state, @inactive_receiver) %></span></div>
           </div>
         </div>
       </div>
@@ -36,5 +43,9 @@ defmodule Open890Web.Live.VFODisplayComponent do
       :memory -> "MEM"
       _ -> "UNKNOWN"
     end
+  end
+
+  def band_for(band_register_state, receiver) do
+    band_register_state |> BandRegisterState.band_for(receiver)
   end
 end
