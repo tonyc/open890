05afe54643c77ba3bbdbafded78e357047d6797f
Tony Collen <tony@halogenlabs.com>
2021-12-28 12:22:28 -0600

    Silly typo

--- mix.exs
+++ mix.exs
@@ -84,7 +84,7 @@ defmodule Open890.MixProject do
       ],
       "assets.deploy.windows": [
         "esbuild default --minify",
-        "cmd assets\\node_modules\\.bin\\postcss assets\\css\\app.scss --output priv\\static\\css\\app.css --verbose --parser psotcss-scss --config assets --use postcss-advanced-variabled postcss-nested autoprefixer",
+        "cmd assets\\node_modules\\.bin\\postcss assets\\css\\app.scss --output priv\\static\\css\\app.css --verbose --parser postcss-scss --config assets --use postcss-advanced-variabled postcss-nested autoprefixer",
         "phx.digest"
       ]
     ]
