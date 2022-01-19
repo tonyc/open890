8a86fddec1c08bca542f95d2e0d900fd04a21cb2
Tony Collen <tony@halogenlabs.com>
2022-01-18 18:43:14 -0600

    Fix view crash when trying to use radio_state.band_scope_edges before it exists

--- lib/open890/tcp_client.ex
+++ lib/open890/tcp_client.ex
@@ -263,18 +263,20 @@ defmodule Open890.TCPClient do
         if (msg |> String.starts_with?("FA") && radio_state.active_receiver == :a) ||
           (msg |> String.starts_with?("FB") && radio_state.active_receiver == :b) do
 
-          {low, high} = radio_state.band_scope_edges
-          delta = radio_state.active_frequency_delta
-          active_receiver = radio_state.active_receiver
+          if radio_state.band_scope_edges do
+            {low, high} = radio_state.band_scope_edges
+            delta = radio_state.active_frequency_delta
+            active_receiver = radio_state.active_receiver
 
-          RadioConnection.broadcast_freq_delta(connection, %{
-            delta: delta,
-            vfo: active_receiver,
-            bs: %{low: low, high: high}
-          })
+            RadioConnection.broadcast_freq_delta(connection, %{
+              delta: delta,
+              vfo: active_receiver,
+              bs: %{low: low, high: high}
+            })
+          end
+
+          RadioConnection.broadcast_radio_state(connection, radio_state)
         end
-
-        RadioConnection.broadcast_radio_state(connection, radio_state)
 
         %{state | radio_state: radio_state}
     end
