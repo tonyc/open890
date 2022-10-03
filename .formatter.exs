ce513190a2c5bc0350dd2c874dd847c7ce7ab062
Tony Collen <tony@halogenlabs.com>
2022-10-03 07:34:22 -0500

    Don't format extract() macros

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [extract: 2, extract: 3]
 ]
